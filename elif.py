45d389b65e6bfe82e8aac66b3c7b4d9c088bafd8
UI-UX-lee <uiuxlee09@gmail.com>
2024-05-20 22:05:21 +0530

    initial commit

---
+++ elif.py
@@ -0,0 +1,7 @@
+mark = int(input("enter the student mark"))
+if(mark>75):
+    print("student is above average" )
+elif(mark>=35):
+    print("student is average")
+else:
+    print("student is fail")    
